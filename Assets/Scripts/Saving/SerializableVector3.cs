8ba8919fa4a54025054566183cefac10bc331934
Ariq Dhia Irfanudin <62055087+1157030004@users.noreply.github.com>
2022-06-06 23:51:11 +0700

    implemented built-in C# serialization API

---
+++ Assets/Scripts/Saving/SerializableVector3.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    [System.Serializable]
+    public class SerializableVector3
+    {
+        float x, y, z;
+
+        public SerializableVector3(Vector3 vector)
+        {
+            this.x = vector.x;
+            this.y = vector.y;
+            this.z = vector.z;
+        }
+
+        public Vector3 ToVector()
+        {
+            return new Vector3(x, y, z);
+        }
+    }
+}
